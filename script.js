4de4cb0032ec497db69c5c5581a91456ad74660e
Julie Ann <julieanniwinski@gmail.com>
2020-11-28 20:38:30 -0500

    Fixed classes to match up with CSS

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+
+//Javascript will not run until HTML finishes loading
 $(document).ready(function(){
 
+    //Displays Current Day     
+    $("#currentDay").text(moment().format("dddd, MMMM Do"));
+    
 
-//Displays Current Day     
- $("#currentDay").text(moment().format("dddd, MMMM Do"));
     
 
 
