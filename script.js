251d1fe36b188d3716c34514bf6ea2fafb22d2d4
Julie Ann <julieanniwinski@gmail.com>
2020-12-03 16:44:21 -0500

    Added color change depend on time function

--- script.js
+++ script.js
@@ -1,16 +1,55 @@
-
-//Javascript will not run until HTML finishes loading
 $(document).ready(function(){
-
+    
+    //Variables
+    var currentHour = moment().hour(); 
+    var num = 8; 
+    var events = []
+    
+    
     //Displays Current Day     
     $("#currentDay").text(moment().format("dddd, MMMM Do"));
     
 
+
+   
     //Save Button Listener
-    $(".saveBTN").on("click", function(){
-        var text = $(this).siblings(".description").val();
-        var time = $(this).parent().attr("id");
-        localStorage.setItem(time,text);
+    $(".saveBtn").on("click", function(){
+        
+       var text = $(this).siblings(".description").val();
+       var time = $(this).parent().attr("id");
+       var dateAdded = moment().format("dddd, MMM Do");
+
+       events.push ({description: text, time: time, date: dateAdded});
+
+       localStorage.setItem("events", JSON.stringify(events));
+       console.log(localStorage)
+    });
+
+
+
+    //Changes colors based on current time
+    $(".time-block").each(function () {
+        var blockHour = parseInt($(this).attr("id"));
+    
+        if (blockHour < currentHour) {
+            $(this).addClass("past");
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+        }
+        else if (blockHour === currentHour) {
+            $(this).removeClass("past");
+            $(this).addClass("present");
+            $(this).removeClass("future");
+        }
+        else {
+            $(this).removeClass("present");
+            $(this).removeClass("past");
+            $(this).addClass("future");
+        }
     })
+
+
+
+  
 });
 
